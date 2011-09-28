dbba507483df9828ed486bf7a58987ecf2e1e856
Jun Wang <nju.wangjun@gmail.com>
2011-09-27 20:15:14 -0400

    arbilloc system call test OK

--- arbiter/syscall-test/hello.c
+++ arbiter/syscall-test/hello.c
@@ -1,5 +1,22 @@
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define __NR_arbilloc	337
+
+/*
+system call arbilloc will take (input + 1) as output
+*/
+
+int arbilloc(int aaa)
+{
+	return syscall(__NR_arbilloc,aaa);
+}
+
 int main()
 { 
-  printf("test\n");
-  return 1;
+	int aaa = 5;
+	printf("if aaa = %d\n", aaa);
+	printf("arbilloc(aaa) =  %d\n", arbilloc(aaa));
+  	return 0;
 }
