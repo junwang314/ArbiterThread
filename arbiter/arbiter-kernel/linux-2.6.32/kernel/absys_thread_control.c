2a28a4f520157857e1d853a6fa593b8311d82c77
Jun Wang <nju.wangjun@gmail.com>
2011-10-10 23:06:24 -0400

    revise absys_thread_control

--- arbiter/arbiter-kernel/linux-2.6.32/kernel/absys_thread_control.c
+++ arbiter/arbiter-kernel/linux-2.6.32/kernel/absys_thread_control.c
@@ -1,42 +1,40 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <asm/atomic.h>
 #include <linux/spinlock.h>
+#include <linux/spinlock_types.h>
 #include <linux/types.h>
+#include <linux/list.h>
 
 #include "absys_thread_control.h"
 
-asmlinkage int sys_absys_thread_control(int ab_thread_control_flag)
-{
-//	spinlock_t ab_thread_control_lock = SPIN_LOCK_UNLOCKED;
-//	static atomic_t ab_identity_count = ATOMIC_INIT(0);
-	static int ab_identity_count = 0;
+DEFINE_RWLOCK(ab_tasklist_lock);
 
-//	spin_lock(&ab_thread_control_lock);	/* critical region starts */
-	if (ab_thread_control_flag == AB_SET_ME_ARBITER) {
-		/* set ab_identity 
-		* with last bit set to 1 to indicate atbiter thread 
-		* ab_tasks should be empty 
-		*/
-//		atomic_inc(&ab_identity_count);
-		current->ab_identity = ((ab_identity_count ++) << AB_IDENTITY_SHIFT) | AB_SET_ME_ARBITER;
+asmlinkage int sys_absys_thread_control(int ab_role)
+{
+	int ab_category = 0;
+	
+	if (ab_role == AB_SET_ME_ARBITER) {
+		/* allocate new ab_identity and just leave ab_tasks empty */
+		ab_category = alloc_category();
 		printk("\nAn arbiter set\n");
 	}
-	if (ab_thread_control_flag == AB_SET_ME_SPECIAL) {
-		/* add ab_tasks to the link list */
-		list_add_tail(&current->ab_tasks, &current->parent->ab_tasks);
+	if (ab_role == AB_SET_ME_SPECIAL) {
+		/* ab_identity should be the same as its parent */
+		ab_category = get_category(current->parent);
 
-		/* ab_identity should be the same as its parent
-		* the last bit should be 0 to indicate it is common thread
-		*/
-		current->ab_identity = current->parent->ab_identity & AB_IDENTITY_MASK;
+		/* add ab_tasks to the doubly linked list */
+		write_lock(&ab_tasklist_lock);
+		list_add(&current->ab_tasks, &current->parent->ab_tasks);
+		write_unlock(&ab_tasklist_lock);
+
 		printk("\nThis is special thread\n");
-
 	}
+	/* set ab_identity */
+	current->ab_identity = convert_to_identity(ab_category, ab_role);
+
 	printk("the ab_identity is set as %x\n",current->ab_identity);
-	printk("the prev and next pointer in ab_task: %p %p\n", current->ab_tasks.prev, current->ab_tasks.next);
-//	spin_unlock(ab_thread_control_lock);	/* critical region ends */
+	printk("list_head ab_tasks: prev-%p current-%p next-%p\n", current->ab_tasks.prev, &current->ab_tasks, current->ab_tasks.next);
 
 	return 0;
 }
--- arbiter/arbiter-kernel/linux-2.6.32/kernel/absys_thread_control.h
+++ arbiter/arbiter-kernel/linux-2.6.32/kernel/absys_thread_control.h
@@ -2,22 +2,55 @@
 #define _ABSYS_THREAD_CONTROL_H
 
 #include <linux/pid.h>
+#include <asm/atomic.h>
 
-#define AB_SET_ME_SPECIAL	0
-#define AB_SET_ME_ARBITER	1
-
-#define AB_IDENTITY_SHIFT	1
-#define AB_IDENTITY_MASK	(-1 << AB_IDENTITY_SHIFT)   //11...11111110
-
-static inline int is_arbiter_task(struct task_struct *tsk)
-{
-	return tsk->ab_identity & AB_SET_ME_ARBITER;
-}
-/*
-static inline int is_special_task(struct task_struct *tsk)
-{
-	return tak->ab_identity & -1 
-}
+/* int ab_identity: 
+* ===============================|=
+*  ab_category (31 bits)     ab_role (1 bit)
 */
+
+#define AB_SET_ME_SPECIAL	0x00000000
+#define AB_SET_ME_ARBITER	0x00000001
+
+#define AB_CATEGORY_SHIFT	1
+#define AB_ROLE_MASK		0x00000001  
+
+static atomic_t ab_category_count = ATOMIC_INIT(0);
+
+static inline int alloc_category(void)
+{
+	atomic_inc(&ab_category_count);
+	return atomic_read(&ab_category_count); 
+}
+
+static inline int get_category(struct task_struct *tsk)
+{
+	return tsk->ab_identity >> AB_CATEGORY_SHIFT;
+}
+
+static inline int get_role(struct task_struct *tsk)
+{
+	return tsk->ab_identity & AB_ROLE_MASK;
+}
+
+static inline int is_arbiter(struct task_struct *tsk)
+{
+	return get_role(tsk);
+}
+
+static inline int is_special(struct task_struct *tsk)
+{
+	return (~get_role(tsk)) & AB_ROLE_MASK;
+}
+
+static inline int is_common(struct task_struct *tsk) /* neither arbiter nor special */
+{
+	return get_category(tsk) != 0;
+}
+
+static inline int convert_to_identity(int ab_category, int ab_role)
+{
+	return (ab_category << AB_CATEGORY_SHIFT) | ab_role;
+}
 
 #endif //_ABSYS_THREAD_CONTROL_H
